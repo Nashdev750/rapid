cf44a95e5827ec5b0b65528700eb94ae97efee68
nahashonkaroki <nahashon.karoki@gliffinglobaltech.com>
2025-04-27 07:27:20 +0300

    v0

--- scrapper2.js
+++ scrapper2.js
@@ -13,7 +13,7 @@ const redis = new Redis({
   const browser = await puppeteer.launch({
     headless: true,
     defaultViewport: { width: 1920, height: 1080 },
-    args: ['--start-maximized']
+    args: ['--start-maximized','--no-sandbox', '--disable-setuid-sandbox']
   });
 
   const page = await browser.newPage();
