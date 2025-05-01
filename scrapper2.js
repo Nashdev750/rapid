125592dbc65393273962098a362774c94bfbab27
nahashonkaroki <nahashon.karoki@gliffinglobaltech.com>
2025-05-01 12:15:39 +0300

    v0

--- scrapper2.js
+++ scrapper2.js
@@ -5,7 +5,7 @@ const { v4: uuidv4 } = require('uuid');
 const redis = new Redis({
   host: 'localhost',
   port: 6379,
-  password: ''
+  password: '43RQ4R45TTTTTT52msh35d1945895fb417p12&565$IYU*776$'
 });
 
 (async () => {
